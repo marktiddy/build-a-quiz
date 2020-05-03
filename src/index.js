4a81e395969228026e3219b70443974beb95bdea
Mark Tiddy <marktiddy@gmail.com>
2020-05-03 18:32:15 +0100

    updating settings

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/build-a-quiz">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
