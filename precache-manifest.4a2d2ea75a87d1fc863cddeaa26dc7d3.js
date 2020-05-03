f114421c0552641a3eae6231fd13027b52b559be
Mark Tiddy <marktiddy@gmail.com>
2020-05-03 10:56:14 +0100

    Updates

---
+++ precache-manifest.4a2d2ea75a87d1fc863cddeaa26dc7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467c5c573b851e66776567b5195108ef",
+    "url": "/build-a-quiz/index.html"
+  },
+  {
+    "revision": "98fc6eb4abb52f6f8c53",
+    "url": "/build-a-quiz/static/css/main.fa68613f.chunk.css"
+  },
+  {
+    "revision": "f188aa994f8767ce6032",
+    "url": "/build-a-quiz/static/js/2.18a92ef4.chunk.js"
+  },
+  {
+    "revision": "f75feb5ff2f098d169d08fee761fb5d9",
+    "url": "/build-a-quiz/static/js/2.18a92ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fc6eb4abb52f6f8c53",
+    "url": "/build-a-quiz/static/js/main.0fa39b27.chunk.js"
+  },
+  {
+    "revision": "3f700d6f5b4ed8966c31",
+    "url": "/build-a-quiz/static/js/runtime-main.74c976db.js"
+  }
+]);
