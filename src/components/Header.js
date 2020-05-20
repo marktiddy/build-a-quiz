76893d984b3ca0a8d4687461c0ec407b7399de38
Mark Tiddy <marktiddy@gmail.com>
2020-05-20 20:17:06 +0100

    Updated components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import Jumbo from './Jumbo';
+
+const Header = () => {
+  return (
+    <>
+      <NavigationBar />
+      <Jumbo />
+    </>
+  );
+};
+
+export default Header;
--- src/screens/QuizPickerScreen.js
+++ src/screens/QuizPickerScreen.js
@@ -13,35 +13,37 @@ const QuizPickerScreen = () => {
   const quizList = state.quizzes;
 
   return (
-    <Container>
-      <Row>
-        {/* Router for this component */}
-        <Switch>
-          <Route path={`${match.path}/:quizId`}>
-            <QuizContent />
-          </Route>
-          <Route path={match.path}>
-            {quizList ? (
-              quizList.map((m) => (
-                <Col
-                  md={6}
-                  xs={12}
-                  key={m.details.id}
-                  className="quiz-picker-col"
-                >
-                  <QuizCard content={m} key={m.details.id} />
+    <>
+      <Container>
+        <Row>
+          {/* Router for this component */}
+          <Switch>
+            <Route path={`${match.path}/:quizId`}>
+              <QuizContent />
+            </Route>
+            <Route path={match.path}>
+              {quizList ? (
+                quizList.map((m) => (
+                  <Col
+                    md={6}
+                    xs={12}
+                    key={m.details.id}
+                    className="quiz-picker-col"
+                  >
+                    <QuizCard content={m} key={m.details.id} />
+                  </Col>
+                ))
+              ) : (
+                <Col>
+                  <p className="loading-quiz">Loading...</p>
+                  <Spinner animation="grow" variant="primary" />
                 </Col>
-              ))
-            ) : (
-              <Col>
-                <p className="loading-quiz">Loading...</p>
-                <Spinner animation="grow" variant="primary" />
-              </Col>
-            )}
-          </Route>
-        </Switch>
-      </Row>
-    </Container>
+              )}
+            </Route>
+          </Switch>
+        </Row>
+      </Container>
+    </>
   );
 };
 
